5a3fab969d18cf60177cfe784f030631d22b807f
lifedaemon-kill <124619126+lifedaemon-kill@users.noreply.github.com>
2023-02-07 23:37:25 +0700

    Создано с помощью силы смерти

---
+++ test2_py.py
@@ -0,0 +1,30 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyMDM62Wm9rzUbSd1of5RowK"
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "yMqPKbcMulqm"
+      },
+      "outputs": [],
+      "source": [
+        "print(\"бимба\")"
+      ]
+    }
+  ]
+}
