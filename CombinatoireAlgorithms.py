51621487cc30fbc3121d8d2139ac9b4cf2d21daf
MrPrograMax <grigorievma9461@gmail.com>
2023-02-07 22:29:58 +0700

    Add Alg 2.1 GenerationOfNbitBinaryVectors

--- CombinatoireAlgorithms.py
+++ CombinatoireAlgorithms.py
@@ -1,6 +1,36 @@
+import numpy as np
+
+
 # region Maxim G.
 
-print("Задача оптимизации")
+# Alg 2.1
+
+def GenerationOfNbitBinaryVectors(n):
+    B = np.zeros(n)
+    while True:
+        if IsContainsOnlyUnit(B):
+            print(f"B contains only unit: B = {B}")
+            break
+        else:
+            print(f"Current B: {B}")
+            m = -1
+            for i in range(len(B)):
+                if B[i] == 0:
+                    m = i
+            B[m] = 1
+            for j in range(m + 1, n):
+                B[j] = 0
+
+
+def IsContainsOnlyUnit(arr):
+    result = True
+    for i in arr:
+        if i != 1:
+            return False
+
+    return True
+
+
 
 # endregion
 
