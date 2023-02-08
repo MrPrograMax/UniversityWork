4f12551b88767c9251b344a17b58f93515be400b
MrPrograMax <grigorievma9461@gmail.com>
2023-02-08 12:57:23 +0700

    Add solve Task "Military bases". Node: need append time, tests, count iters

--- CombinatoireAlgorithms.py
+++ CombinatoireAlgorithms.py
@@ -1,18 +1,19 @@
+import sys
 import numpy as np
 import pandas as pd
+import itertools as iter
 
 
-# region Maxim G.
+# region Algorithms
 
 # Alg 2.1
-
-def GenerationOfNbitBinaryVectors(n, flagOnPrint = False):
+def GenerationOfNbitBinaryVectors(n, flagOnPrint=False):
     B = np.zeros(n)
     countOperation = 0
     while True:
-        if IsContainsOnlyUnit(B):
+        if IsContainsOnlyUnits(B):
             if flagOnPrint:
-                print(f"B contains only unit: B = {B}")
+                print(f"B contains only units: B = {B}")
             break
         else:
             countOperation += 1
@@ -29,7 +30,7 @@ def GenerationOfNbitBinaryVectors(n, flagOnPrint = False):
     return countOperation
 
 
-def IsContainsOnlyUnit(arr):
+def IsContainsOnlyUnits(arr):
     for i in arr:
         if i != 1:
             return False
@@ -37,8 +38,48 @@ def IsContainsOnlyUnit(arr):
     return True
 
 
-# Test count of operation:
-def Test(end):
+def GetInfoAboutBastMilitaryBase(numberOfAreas, neighborhoodMatrix, losses):
+    minSum = sys.maxsize * 2 + 1  # int max value
+    indexBase = []
+    keys = range(numberOfAreas)
+
+    for i in range(numberOfAreas, 1, -1):
+        combinations = list(iter.combinations(keys, i))
+
+        for curKeys in combinations:
+            vector = GetSumOfVectors(neighborhoodMatrix, curKeys)
+            if IsNoneZero(vector):
+                sum = 0
+                for k in curKeys:
+                    sum += losses[k]
+                if sum < minSum:
+                    minSum = sum
+                    indexBase = curKeys
+
+    print(f"Минимальная сумма: {minSum}\nРасположение баз: {indexBase}\nМинимальное кол-во баз: {len(indexBase)}")
+
+
+def IsNoneZero(arr):
+    for i in arr:
+        if i == 0:
+            return False
+
+    return True
+
+
+def GetSumOfVectors(arrays, keys):
+    result = [0] * len(arrays)
+    for i in keys:
+        result += arrays[i]
+
+    return result
+
+
+# endregion
+
+# region Tests
+
+def TestAlg21(end):
     end += 1
     operations = []
     n = range(1, end)
@@ -54,17 +95,26 @@ def Test(end):
     print(data)
 
 
-Test(20)
 # endregion
 
+# region Main
 
-# region Maxim D.
+TestAlg21(20)
+
+A = np.array([
+    [1, 1, 0, 1, 0, 0, 0, 0, 0],
+    [1, 1, 1, 0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 0, 0, 1, 0, 0, 0],
+    [1, 0, 0, 1, 1, 0, 1, 0, 0],
+    [0, 1, 0, 1, 1, 1, 0, 1, 0],
+    [0, 0, 1, 0, 1, 1, 0, 0, 1],
+    [0, 0, 0, 1, 0, 0, 1, 1, 0],
+    [0, 0, 0, 0, 1, 0, 1, 1, 1],
+    [0, 0, 0, 0, 0, 1, 0, 1, 1]
+])
 
 
-# endregion
-
-
-# region Ruslan Z.
-
+losses = [0.1, 1, 0.1, 1, 0.1, 1, 0.1, 1, 0.1]
+GetInfoAboutBastMilitaryBase(9, A, losses)
 
 # endregion
