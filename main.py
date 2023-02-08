590bca98040787167391f0ac5e4855e864e35d3d
MrPrograMax <grigorievma9461@gmail.com>
2023-02-08 15:03:01 +0700

    little correct

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import numpy as np
 import pandas as pd
 import itertools as iter
 
+from Tests.TestAlgorithm21 import *
+from Tests.TestMilitaryBases import *
+
 # region Algorithm
+
 
 # endregion
 
