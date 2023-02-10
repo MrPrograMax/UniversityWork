8edee34024b44a2229113b045c6abe65eb1e0f70
MrPrograMax <grigorievma9461@gmail.com>
2023-02-10 15:11:48 +0700

    main for local test

--- main.py
+++ main.py
@@ -3,8 +3,4 @@ import numpy as np
 import pandas as pd
 import itertools as iter
 
-b = np.array([[]])
-b = np.append(b, [[11, 13]], axis=1)
-b = np.append(b, [[12, 14]], axis=0)
-b = np.append(b, [[19, 19]], axis=0)
-print(len())
+#file for test
