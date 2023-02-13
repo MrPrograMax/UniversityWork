f5c05528661a4e098de0970551829675b6b095ad
lifedaemon-kill <124619126+lifedaemon-kill@users.noreply.github.com>
2023-02-13 11:19:40 +0700

    Add Valid test

--- CombinatoireAlgorithms/Tasks/RegularSet/TestRegularSet.py
+++ CombinatoireAlgorithms/Tasks/RegularSet/TestRegularSet.py
@@ -1,1 +1,13 @@
 from ResgularSet import *
+
+print("ТЕСТИРОВАНИЕ АЛГОРИТМА\n")
+
+print("Три точки образующие правильный треугольник")
+a = Point(0, 0)
+b = Point(5, 5 * math.sqrt(3))
+c = Point(10, 0)
+array = [a, b, c]
+if IsRegularSet(array):
+  print(f"Множество {array}\nРегулярно")
+else:
+  print(f"Множество {array}\nНерегулярно)
