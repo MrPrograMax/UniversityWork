44200f3c76d4e53f081e52c60b96b56ad39db505
lifedaemon-kill <124619126+lifedaemon-kill@users.noreply.github.com>
2023-03-13 12:54:59 +0700

    Updated depth crindgil'ku, poka ne working yet
    Цикл пока не понимает зачем существует

--- CombinatoireAlgorithms/Algorithms/Algorithm51.py
+++ CombinatoireAlgorithms/Algorithms/Algorithm51.py
@@ -1,5 +1,37 @@
 print("hello im alg51, but i dont warking yet! ")
 
-def depth_first_search(arrray:list):
-    return 0
+import numpy as np
+print("hello im alg51, but i dont warking yet!")
 
+def depth_search(L):
+    n = len(L)
+    stack=[]
+
+    for i in range(n):
+        stack.append(i)
+        x = [i]
+        for j in range(n):
+            if j not in x and :
+                stack.append(j)
+                x.append(j)
+
+    return "Пока возвращять нечего, печальное зрелище"
+
+Links = np.array([
+    [0, 1, 1, 0, 1, 0, 0],
+    [1, 0, 0, 1, 1, 0, 0],
+    [1, 0, 0, 0, 1, 0, 0],
+    [0, 1, 0, 0, 1, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 1, 0, 1],
+    [0, 0, 0, 0, 1, 1, 0]
+])
+LN = [
+    [1,2,4],
+    [0,3,4],
+    [0,4],
+    [1,4],
+    [0,1,2,3,4,5,6],
+    [4,6],
+    [4,5]
+]
