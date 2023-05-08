2c0138b3ad2221ad80d91e313da1c49e0a7552b2
lifedaemon-kill <124619126+lifedaemon-kill@users.noreply.github.com>
2023-05-08 09:14:46 +0000

    upd alg for less cringe

--- CombinatoireAlgorithms/Tasks/Network work planning/Network_planning.py
+++ CombinatoireAlgorithms/Tasks/Network work planning/Network_planning.py
@@ -83,9 +83,7 @@ def floyd_critical(W, start_point: int = 0, end_point: int = None) -> tuple:
             if W[i][j] != np.inf and i != j:
                 H[i][j] = i
 
-    k = -1
-    while True:
-        k += 1
+    for k in range(n):
         # 2 Построение Н, D
         for i in range(n):
             for j in range(n):
@@ -101,8 +99,7 @@ def floyd_critical(W, start_point: int = 0, end_point: int = None) -> tuple:
             if D[i][i] < 0:
                 error_flag = True
                 break
-        if k == n-1:
-            break
+
     # 7.4 Нахождение пути из Н
 
     if error_flag is False and D[start_point, end_point] != np.inf:
