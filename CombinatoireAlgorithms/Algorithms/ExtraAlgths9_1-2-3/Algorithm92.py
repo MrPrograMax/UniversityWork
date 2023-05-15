5fb999056a8013b0204eb88f4e101974c4cf8893
lifedaemon-kill <124619126+lifedaemon-kill@users.noreply.github.com>
2023-05-15 06:12:22 +0000

    complited

--- CombinatoireAlgorithms/Algorithms/ExtraAlgths9_1-2-3/Algorithm92.py
+++ CombinatoireAlgorithms/Algorithms/ExtraAlgths9_1-2-3/Algorithm92.py
@@ -1,6 +1,18 @@
-"""
-Функция wst(k, delta)
-"""
+def wst(k:int, delta, probs:list):
+    """
+    k - count of iteration and dimension
+    delta - the sum of the last two numbers
+    prob - probabilities
 
-def wst(k:int, delta):
-    print("sorry bruh, im useless function yet")
+    returns a pointer to where the insert was made
+    """
+
+    probs[k-1] = delta
+
+    for i in range(k - 1, 1, -1):
+        #от к-1 до 2 в обратном порядке
+        if probs[i - 1] < probs[i]:
+            probs[i - 1], probs[i] = probs[i], probs[i - 1]
+        else:
+            break
+    return i
