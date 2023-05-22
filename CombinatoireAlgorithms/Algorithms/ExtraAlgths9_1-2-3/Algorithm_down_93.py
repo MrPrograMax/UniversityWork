28a6dd77ae20690555f5d76cb1f1885dc9aa922b
lifedaemon-kill <124619126+lifedaemon-kill@users.noreply.github.com>
2023-05-22 03:34:54 +0000

    add down func

--- CombinatoireAlgorithms/Algorithms/ExtraAlgths9_1-2-3/Algorithm_down_93.py
+++ CombinatoireAlgorithms/Algorithms/ExtraAlgths9_1-2-3/Algorithm_down_93.py
@@ -2,9 +2,29 @@
 Функция down(k, j)
 """
 
-def down(k:int, j:int):
+def down(k:int, j:int, C, L):
     """
     k - dimension and iterations
     j - insertion pointer
+    return C,L
     """
-    print("sorry bruh, im useless function yet")
+    #1
+    beta   = C[j, : ]
+    length = L[j]
+
+    #2
+    for i in range(j, k-2):
+        C[i, :] = C[i+1, :]
+        L[i]= L[i + 1]
+
+    #3
+    C[k-1, : ] = beta
+    C[ k , : ] = beta
+
+    #4
+    C[k-1, length + 1] = 0
+    C[k  , length + 1] = 1
+    L[k-1] = length + 1
+    L[ k ] = length + 1
+
+    return C, L
