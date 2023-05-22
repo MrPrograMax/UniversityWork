8a7f1e922afa75ca39abf4f98e70bd05376aec0f
lifedaemon-kill <124619126+lifedaemon-kill@users.noreply.github.com>
2023-05-22 06:40:47 +0000

    upd smth is working but smth not

--- CombinatoireAlgorithms/Tasks/Text compression/text_compression.py
+++ CombinatoireAlgorithms/Tasks/Text compression/text_compression.py
@@ -13,7 +13,7 @@ def down(k:int, j:int, matrix_c, length_p) -> tuple:
     """
     #1
     beta   = matrix_c[j, : ]
-    length = length_p[j]
+    length = int(length_p[j])
 
     #2
     for i in range(j, k-2):
@@ -21,14 +21,14 @@ def down(k:int, j:int, matrix_c, length_p) -> tuple:
         length_p[i]  = length_p[i + 1]
 
     #3
+    matrix_c[k - 2, : ] = beta
     matrix_c[k - 1, : ] = beta
-    matrix_c[  k  , : ] = beta
 
     #4
-    matrix_c[k - 1, length + 1] = 0
-    matrix_c[  k  , length + 1] = 1
+    matrix_c[k - 2, length + 1] = 0
+    matrix_c[k - 1, length + 1] = 1
+    length_p[k - 2] = length + 1
     length_p[k - 1] = length + 1
-    length_p[  k  ] = length + 1
 
     return matrix_c, length_p
 
@@ -40,15 +40,16 @@ def wst(k:int, delta, probs) -> tuple:
 
     возвращает указатель на место вставки
     """
-    probs[k-1] = delta
+    probs[k-2] = delta
 
-    for i in range(k - 1, 1, -1):
+    for i in range(k-2, 0, -1):
         #от к-1 до 2 в обратном порядке
         if probs[i-1] < probs[i]:
             probs[i-1], probs[i] = probs[i], probs[i-1]
         else:
             break
-
+        
+    print(f'probs{probs}')
     return i, probs
 
 def huffman(letters_p, k : int = 2) -> tuple:
@@ -65,9 +66,10 @@ def huffman(letters_p, k : int = 2) -> tuple:
         length_p[0] = 1
         length_p[1] = 1
     else:
-        delta = letters_p[k-1] + letters_p[k]
+        delta = letters_p[k-2] + letters_p[k-1]
         j, letters_p = wst(k, delta, letters_p)
         matrix_c, length_p = down(k, j, matrix_c, length_p)
+        matrix_c, length_p = huffman(letters_p, k-1)
 
     return matrix_c, length_p
 
@@ -83,7 +85,7 @@ alph = set(POEM[:])
 len_alph = len(alph)
 
 dic_letters = {letter: POEM.count(letter)/LENGTH_POEM for letter in alph}
-dic_letters = dict(sorted(dic_letters.items(), key= lambda x : x[1]))
+dic_letters = dict(sorted(dic_letters.items(), key = lambda x : x[1], reverse=True))
 print(dic_letters)
 
 letter_probability = np.array(list(dic_letters.values()))
@@ -92,7 +94,7 @@ print(letter_probability)
 
 print(sum(letter_probability))
 
-C, L = huffman(letter_probability, 2)
+C, L = huffman(letter_probability, len_alph)
 
 print(f'C =\n{C}')
 print(f'L =\n{L}')
